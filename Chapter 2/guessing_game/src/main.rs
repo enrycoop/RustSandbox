f55e157fcddbc1cca8fa39c5a81be08c07e40290
enricocoppolecchia <enrycoop92@gmail.com>
2024-05-02 07:01:10 +0200

    Chapter 6: match control flow construct

--- Chapter 2/guessing_game/src/main.rs
+++ Chapter 2/guessing_game/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
 
     let secret_number = rand::thread_rng()
         .gen_range(1..=100);
-    // println!("The secret number is: {secret_number}");
+    println!("The secret number is: {secret_number}");
     loop {
         println!("Please input your guess.");
 
--- Chapter 6/match_control_flow_construct/src/main.rs
+++ Chapter 6/match_control_flow_construct/src/main.rs
@@ -1,22 +1,64 @@
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
     match coin {
-        Coin::Penny => 1,
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        },
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        },
     }
 }
 
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+    
+
+fn add_fancy_hat() {}
+fn remove_fancy_hat() {}
+fn move_player(num_spaces: u8) {}
+fn reroll() {}
+
+
 fn main() {
-    println!("{}", value_in_cents(Coin::Penny));
-    println!("{}", value_in_cents(Coin::Nickel));
-    println!("{}", value_in_cents(Coin::Dime));
-    println!("{}", value_in_cents(Coin::Quarter));
+    println!("{}", value_in_cents(Coin::Quarter(UsState::Alaska)));
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    let dice_roll = 9;
+    // match dice_roll {
+    //     3 => add_fancy_hat(),
+    //     7 => remove_fancy_hat(),
+    //     other => move_player(other),
+    // }
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        _ => (),
+    }
+
 }
