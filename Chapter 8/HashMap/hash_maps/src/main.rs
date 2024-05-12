4265f2bb2a92c21912e1730154a56f2ad53fa48a
enrycoop <enrycoop92@gmail.com>
2024-05-12 10:31:29 +0200

    feat: chapter 8 complete

--- Chapter 8/HashMap/hash_maps/src/main.rs
+++ Chapter 8/HashMap/hash_maps/src/main.rs
@@ -24,4 +24,42 @@ fn main() {
     for (key, value) in &scores {
         println!("{key}: {value}");
     }
+
+    // Ownership
+    let field_name = String::from("Favourite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, &field_value);
+    // field_name e field_value non sono più valide poichè è stata fatta la move
+    //println!("{}", field_name);
+
+    // UPDATING A VALUE
+
+    // overwriting
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Blue"), 25);
+
+    println!("{:?}", scores);
+
+    // adding if a key isn't present
+    scores.entry(String::from("orange")).or_insert(50);
+    scores.entry(String::from("orange")).or_insert(50);
+    
+    println!("{:?}", scores);
+
+    // updating a value based on the old value
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+    
+    for word in text.split_whitespace() {
+      let count = map.entry(word).or_insert(0);
+      *count += 1;
+    } // count è fuori scope quindi è safe
+
+    println!("{:?}", map);
+
 }
