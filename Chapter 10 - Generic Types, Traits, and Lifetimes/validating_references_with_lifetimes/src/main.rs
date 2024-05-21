eea660de884055a8e0c5bc51bd05861dc91a5704
enrycoop <enrycoop92@gmail.com>
2024-05-21 09:03:59 +0200

    cap 10

--- Chapter 10 - Generic Types, Traits, and Lifetimes/validating_references_with_lifetimes/src/main.rs
+++ Chapter 10 - Generic Types, Traits, and Lifetimes/validating_references_with_lifetimes/src/main.rs
@@ -1,3 +1,32 @@
+// ORIGINAL
+// fn longest(x: &str, y: &str) -> &str {
+//     if x.len() > y.len() {
+//         x
+//     } else {
+//         y
+//     }
+// }
+// WITH LIFETIME
+/* In pratica, significa che il lifetime della reference restituita da longest
+è la stessa della lifetime di 'a.
+Da ricordare che, quando specifichiamo il parametro lifetime non stiamo cambiando
+la lifetime di ognuno dei falori passati o restituiti, piuttosto, stiamo specificando
+al borrow checker che deve rifiutare ogni valore che non aderisce a questi vincoli.
+Quando annotiamo la lifetime nelle funzioni, le annotazioni vanno nella signature,
+non nel body. Le annotazioni lifetime diventano parte del contratto della funzione.
+Avere nelle signature delle funzioni i contratti significa che l'analisi che il 
+compilatore rust deve fare è semplice.
+ */
+fn longest<'a>(x: &'a str, y: &str) -> &'a str {
+    x
+}
+
 fn main() {
-    
+    let string1 = String::from("long string is long");
+    let result;
+    {
+        let string2 = String::from("xyz");
+        result = longest(string1.as_str(), string2.as_str());
+    }
+    println!("The longest string is {}", result);
 }
